82edeb9d1767fc2266c2dc4da367af2374de5120
Praypit <121992826+Praypit@users.noreply.github.com>
2023-01-05 09:55:21 +0530

    Update zphisher.sh

--- zphisher.sh
+++ zphisher.sh
@@ -212,7 +212,8 @@ banner() {
 		${ORANGE}║║──║║║╔╗║╚═╝║╚╝║║╚╗ 
 		${ORANGE}╚╝──╚╝╚╝╚╩═╗╔╣╔═╩╩═╝                                    
 		${ORANGE}─────────╔═╝║║║
-                ${ORANGE}─────────╚══╝╚╝
+                ${ORANGE}─────────╚══╝╚╝                ${RED}Version : ${__version__}
+
                 ${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by Abhi thakur (advance fishing tool)${WHITE}
 	EOF
 }
